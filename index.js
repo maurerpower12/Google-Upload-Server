da7fdac096892c0ec7526d4342a8d2f96687dfec
Joe Maurer <josephmau12@gmail.com>
2024-09-16 21:19:26 -0700

    feat: organize scripts

--- index.js
+++ index.js
@@ -4,29 +4,20 @@ const { google } = require('googleapis');
 const fs = require('fs');
 const path = require('path');
 const axios = require('axios');
-const photos = require('googlephotos');
+const Photos = require('googlephotos');
 const { exec } = require('child_process');
 
 const app = express();
 const port = 3000;
+// Load Google OAuth2 credentials
+const CREDENTIALS_PATH = path.join(__dirname, 'credentials.json');
+const TOKEN_PATH = 'token.json';
+const SCOPES = [Photos.Scopes.READ_ONLY, Photos.Scopes.READ_AND_APPEND];
+const ALBUM_ID = 'AJpAlTpor0z34OUMyZcIDm0sEDgnBh76mzMTKGk_zoy9nuRRXJt9jdhWO1G_1Wn0cjRvWbzqmZjg';
 
-const openBroswer = (url) => {
-  const startCmd = {
-    darwin: 'open', //macOS
-    win32: 'start', // windows
-    linux: 'xdg-open' // linux
-  }[process.platform]
+let authClient;
+let photos;
 
-  if (startCmd) {
-    exec(`${startCmd} ${url}`, (error) => {
-      if (error) {
-        console.error('Failed to open broswer to: ', url, error);
-      }
-    });
-  } else {
-    console.error('Platform not supported');
-  }
-}
 app.listen(port, async () => {
   await authorize();
   console.log(`Server listening on port http://localhost:${port}`);
@@ -45,7 +36,7 @@ const storage = multer.diskStorage({
   filename: (req, file, cb) => {
     console.log("File: ", file);
     var fileName = "defaultFileName";
-    const fileExt = ".jpg";
+    const fileExt = ".png";
 
     if (file.originalname) {
       fileName = file.originalname;
@@ -60,14 +51,6 @@ const storage = multer.diskStorage({
 })
 const upload = multer({ storage: storage, preservePath: true });
 
-// Load Google OAuth2 credentials
-const CREDENTIALS_PATH = path.join(__dirname, 'credentials.json');
-const TOKEN_PATH = 'token.json';
-const SCOPES = ['https://www.googleapis.com/auth/photoslibrary'];
-const ALBUM_ID = 'AF1QipPeUoVGbAPDVJlVif6DMYfRWArLc7FL7z6i02OK';
-
-let authClient;
-
 async function authorize() {
     const credentials = JSON.parse(fs.readFileSync(CREDENTIALS_PATH, 'utf8'));
     const { client_secret, client_id, redirect_uris } = credentials.web;
@@ -76,6 +59,7 @@ async function authorize() {
     if (fs.existsSync(TOKEN_PATH)) {
         const token = fs.readFileSync(TOKEN_PATH, 'utf8');
         oAuth2Client.setCredentials(JSON.parse(token));
+        photos = new Photos(JSON.parse(token).access_token);
     } else {
         // Get and store new token (manual step)
         const authUrl = oAuth2Client.generateAuthUrl({ access_type: 'offline', scope: SCOPES });
@@ -88,23 +72,55 @@ async function authorize() {
 app.post('/api/upload', upload.single('photo'), async (req, res) => {
   console.log("[UPLOAD API] called: ", req.body);
   console.log("Uploaded: ", req.file);
-  res.status(200).json({ success: true });
-    // try {
-    //     const filePath = req.file.path;
+  
+  try {
+      const filePath = req.file.path;
 
-    //     if (!authClient) await authorize();
+      if (!authClient) await authorize();
 
-    //     const mediaItemId = await uploadPhoto(authClient, filePath, ALBUM_ID);
+      uploadToGooglePhotos(authClient, filePath, ALBUM_ID);
+      res.status(200).json({ success: true });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ success: false, error: error.message });
+    }
+});
 
-    //     res.status(200).json({ success: true, mediaItemId });
-    // } catch (error) {
-    //     console.error(error);
-    //     res.status(500).json({ success: false, error: error.message });
-    // } finally {
-    //     fs.unlink(req.file.path, (err) => {
-    //         if (err) console.error('Failed to delete temporary file:', err);
-    //     });
-    // }
+app.get('/api/getFolderName', async (req, res) => {
+  console.log("[UPLOAD getFolderName API] called");
+  
+  try {
+      if (!authClient) await authorize();
+
+      const response = await photos.albums.get(ALBUM_ID);
+      console.log('Album Name:', response);
+      //listAlbums();
+      res.status(200).json({ success: true });
+    } catch (error) {
+      console.error(error);
+      if (error.response) {
+        const errorResponse = await error.response.json();
+        console.error('API error details:', errorResponse);
+      }
+      res.status(500).json({ success: false, error: error.message });
+    }
+});
+
+app.get('/api/listAllAlbums', async (req, res) => {
+  console.log("[UPLOAD listAllAlbums API] called");
+  
+  try {
+      if (!authClient) await authorize();
+      await listAlbums();
+      res.status(200).json({ success: true });
+    } catch (error) {
+      console.error(error);
+      if (error.response) {
+        const errorResponse = await error.response.json();
+        console.error('API error details:', errorResponse);
+      }
+      res.status(500).json({ success: false, error: error.message });
+    }
 });
 
 app.get('/auth/google/callback', async (req, res) => {
@@ -117,6 +133,8 @@ app.get('/auth/google/callback', async (req, res) => {
       const { tokens } = await authClient.getToken(code);
       authClient.setCredentials(tokens);
 
+      photos = new Photos(tokens.access_token);
+
       // Store the token for later use
       fs.writeFileSync(TOKEN_PATH, JSON.stringify(tokens));
 
@@ -127,73 +145,45 @@ app.get('/auth/google/callback', async (req, res) => {
   }
 });
 
-async function uploadPhoto(authClient, filePath, albumId) {
-    const fileName = path.basename(filePath);
-
-    // Step 1: Upload the image to Google Photos to get an upload token
-    const uploadToken = await getUploadToken(authClient, filePath);
-
-    // Step 2: Use the upload token to create a media item
-    const mediaItemId = await createMediaItem(authClient, uploadToken, albumId, fileName);
-
-    return mediaItemId;
+async function uploadToGooglePhotos(authClient, filePath, albumId) {
+  try {
+    await photos.mediaItems.upload(albumId, "name", filePath, "description");
+  } catch (err) {
+    console.error('The API returned an error:', err.message);
+    if (err.response) {
+      const errorResponse = await err.response.json();
+      console.error('API error details:', errorResponse);
+    }
+  } 
 }
 
-async function getUploadToken(authClient, filePath) {
-    const fileStream = fs.createReadStream(filePath);
-    const fileSize = fs.statSync(filePath).size;
-    const mimeType = 'image/png';
+const openBroswer = (url) => {
+  const startCmd = {
+    darwin: 'open', //macOS
+    win32: 'start', // windows
+    linux: 'xdg-open' // linux
+  }[process.platform]
 
-    try {
-      const response = await axios.post('https://photoslibrary.googleapis.com/v1/uploads', fileStream, {
-          headers: {
-              'Authorization': `Bearer ${authClient.credentials.access_token}`,
-              'Content-Type': 'application/octet-stream',
-              'X-Goog-Upload-Content-Type': mimeType,
-              //'X-Goog-Upload-File-Name': path.basename(filePath),
-              'X-Goog-Upload-Protocol': 'raw',
-              'Content-Length': fileSize
-          }
-      });
-
-      const uploadToken = response.data;
-      console.log("Upload Token: " + uploadToken);
-      return uploadToken;
-  } catch (error) {
-    console.error('Error uploading photo:', error);
-    throw error;
+  if (startCmd) {
+    exec(`${startCmd} ${url}`, (error) => {
+      if (error) {
+        console.error('Failed to open broswer to: ', url, error);
+      }
+    });
+  } else {
+    console.error('Platform not supported');
   }
 }
 
-async function createMediaItem(authClient, uploadToken, albumId, fileName) {
+async function listAlbums() {
   try {
-    const response = await axios.post('https://photoslibrary.googleapis.com/v1/mediaItems:batchCreate', {
-        albumId: albumId,
-        newMediaItems: [
-            {
-              description: 'Photo booth picture',
-              simpleMediaItem: {
-                  fileName: 'photo1.jpg', //fileName,
-                  uploadToken: uploadToken
-              }
-            }
-        ]
-    }, {
-        headers: {
-            'Authorization': `Bearer ${authClient.credentials.access_token}`,
-            'Content-Type': 'application/json'
-        }
-    });
-
-    const mediaItemResult = response.data.newMediaItemResults[0];
-    if (mediaItemResult.status && mediaItemResult.status.code === 0) {
-      console.log("Upload Media : " + JSON.stringify(mediaItemResult));
-        return mediaItemResult.mediaItem.id;
-    } else {
-        throw new Error('Error creating media item: ' + mediaItemResult.status.message);
+    const albumsResponse = await photos.albums.list(50);
+    console.log('Albums:', albumsResponse);
+  } catch (err) {
+    console.error('The API returned an error:', err.message);
+    if (err.response) {
+      const errorResponse = await err.response.json();
+      console.error('API error details:', errorResponse);
     }
-  } catch (error) {
-    console.error('Error uploading photo media:', error);
-    throw error;
   }
 }
